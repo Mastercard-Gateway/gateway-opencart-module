07dcbe5723f300086f584a848629e85529dc7491
Kristjan Heinaste <kristjan@ontapgroup.com>
2020-04-27 15:05:48 +0300

    Initial import

---
+++ catalog/language/en-gb/extension/payment/mpgs_hosted_checkout.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright (c) 2020 Mastercard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Text
+$_['text_credit_card']               = 'Credit Card Details';
+$_['text_card_type']                 = '%s';
+$_['text_card_label']                = 'ending in %s';
+$_['text_card_expiry']               = '(expires %s)';
+$_['text_use_new_card']              = 'Use a new payment method';
+$_['text_save_card']                 = 'Save to account';
+$_['text_secure_payment']            = 'Processing Secure Payment';
+
+// Entry
+$_['entry_cc_number']                = 'Card number';
+$_['entry_expiry_month']             = 'Expiry (MM)';
+$_['entry_expiry_year']              = 'Expiry (YY)';
+$_['entry_security_code']            = 'Card Code';
+$_['entry_cardholder_name']          = 'Name on Card';
+
+// Error
+$_['error_card_number']              = 'Invalid Card Number';
+$_['error_expiry_month']             = 'Invalid Expiry Month';
+$_['error_expiry_year']              = 'Invalid Expiry Year';
+$_['error_security_code']            = 'Invalid Security Code';
+$_['error_payment_declined']         = 'Payment has declined';
+$_['error_token_not_present']        = 'Token not present in response';
+$_['data_error']                     = 'Data Error';
+$_['error_unexpected']               = 'Unexpected payment condition error.';
